0b0d38c7cae56dbfc2fd4e6e886be5a5fa9700af
Jayanth2323 <jayanthchennoju@gmail.com>
2025-02-18 23:17:15 +0530

    first co

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+<<<<<<< HEAD
 import { MainComponent } from './main/main.component';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent, FooterComponent, MainComponent],
+=======
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+>>>>>>> a60a0e4 (Second Commit)
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
